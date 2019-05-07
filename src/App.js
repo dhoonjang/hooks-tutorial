394a92f63064b9f270a52f3343267caf4188e027
dhoonjang <dhoonjang@dhoonjangui-MacBookPro.local>
2019-05-07 15:32:39 +0900

    learn input state with useReducer, useMemo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 // import Info from './Info';
 import Context from './Context';
 import Counter from './Counter';
+import Info from './Info';
+import Average from './Average';
 
 const App = () => {
   // const [visible, setVisible] = useState(false);
@@ -9,6 +11,8 @@ const App = () => {
     <>
       <Context/>
       <Counter/>
+      <Info/>
+      <Average/>
     </>
   );
 };
